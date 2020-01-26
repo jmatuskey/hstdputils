6fa77d7d0ec89506e4fc9c9ca602ff23113dd17a
EC2 Default User <ec2-user@ip-172-31-46-202.ec2.internal>
2020-01-26 21:06:17 +0000

    Work on processing lists of ipppssoots and s3 outputs.

--- hstdputils/process.py
+++ hstdputils/process.py
@@ -19,18 +19,35 @@ def process(ipppssoot, output_bucket=None, prefix=None):
     otherwise immense bucket.
     """
     files = download.download(ipppssoot)
+
     info = planner.id_info(ipppssoot)
+
     for filename in planner.process_files(info, files):
         bestrefs.assign_bestrefs(filename)
         err = os.system(info.executable + " " + filename)
+
     all = glob.glob("*.fits")
-    outputs = set(all) - set(files)
-    if output_bucket:
-        for filename in outputs:
-            s3.upload_filename(filename, output_bucket, prefix=prefix)
+    outputs = list(set(all) - set(files))
+    
+    output_files(outputs, output_bucket, prefix)
+    
     return outputs
+
+def process_ipppssoots(ipppssoots, output_bucket=None, prefix=None):
+    for ipppssoot in ipppssoots:
+        process(ipppssoot, output_bucket, prefix)
+
+# -----------------------------------------------------------------------------
+
+def output_files(files, output_bucket=None, prefix=None):
+    if output_bucket:
+        for filename in files:
+            s3.upload_filename(filename, output_bucket, prefix=prefix)
 
 # -----------------------------------------------------------------------------
 
 if __name__ == "__main__":
-    process(*sys.argv[1:])
+    output_bucket = sys.argv[1]
+    prefix = sys.argv[2]
+    ipppssoots = sys.argv[3:]
+    process_ipppssoots(ipppssoots, output_bucket, prefix)
